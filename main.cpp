a72be69537ee2ebe4697da2a4efa14adf6824317
George <georgenechifor01@gmail.com>
2025-05-23 15:05:19 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include<iostream>
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+
+        window.display();
+    }
+}
